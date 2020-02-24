052adc801ff484f6784c533c4df88b19a54c895c
Mauricio Perez <ing.mauricioprz@gmail.com>
2020-02-23 22:58:58 -0600

    primeros cambios

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './img/remcon-free.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+         <p>
+         En Remodelacion
+       </p>
+       <p>
+       Contacto:<br>
+       </br>
+       +52 (55) 5596 2058 // +52 (55) 2106 2206<br></br>
+       remcon07@hotmail.com
+       </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
